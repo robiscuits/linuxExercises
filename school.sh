46f5739950891d636cc3260b6d2493899b6788ea
WhiteForest7 <91106562+WhiteForest7@users.noreply.github.com>
2022-03-08 17:06:53 -0600

    average

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
 #/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" |cut -d , -f 7 | { sum=0;num=0; while read n; do sum=$(($sum + $n));num=$(($num+1)) ; done; a=$(echo "scale=6; $sum/$num" | bc); echo $a; }
