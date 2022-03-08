9a2475ab3f06fc6eee15f09b73a17a6d60b8f35a
Evan O'Keefe <eokeefe3@wisc.edu>
2022-03-08 12:46:35 -0600

    Added usage statement to rm_n

--- rm_n.sh
+++ rm_n.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+ #!/bin/bash
 
 dir=$1
 n=$2
 
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+
 for i in $(find $dir -type f -size $n); do
-  rm "$i"
+    rm "$i"
 done
