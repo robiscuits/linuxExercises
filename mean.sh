92e6716b2c3d338b5c494cf428bd185d131e20ea
Robert George <rrgeorge@wisc.edu>
2022-03-08 11:58:19 -0600

    Finished mean.sh

--- mean.sh
+++ mean.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [[ ($# -eq 0) || ($# -gt 2) ]]; then # Recall: "-ne" checks integer inequality.
+    echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)
+    exit 0
 
+elif [[ $# -eq 1 ]]; then # Recall: "-ne" checks integer inequality.
+    column=$1
+    file=/dev/stdin
+else
+    column=$1
+    file=$2
+fi
+
+cut -d "," -f $column $file | tail -n +2 | { count=0 sum=0; while read n; do count=$(($count + 1)) sum=$(($sum + $n)); done; mean=$(($sum / $count)); echo $mean; }
